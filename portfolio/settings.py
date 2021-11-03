bc3c0699d4eddf1617b6ff93a6e2a65a64d22bfe
VaneManzanelli <vanemanza1@gmail.com>
2021-11-03 19:08:46 -0300

    eliminé archivos de Heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ SECRET_KEY = 'django-insecure-o=h7sg$60z9_332u97vp9%5v=)ftk+sd-mpt)_1^fz46mo=i&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -151,5 +150,5 @@ EMAIL_HOST_PASSWORD = "VaneDev2021"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+
 
